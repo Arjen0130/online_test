e5e81f62d75dbb29471768df45e6097c486a58ab
zhangrunjie <zhangrunjie@bytedance.com>
2018-09-16 20:27:30 +0800

    添加一道与大数乘法相关的题目的描述，但是还没有开始解题。

---
+++ 2018-09-15-xiaohongshu/3_count_zero_at_rear.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+//STL head file
+#include <vector>
+#include <queue>
+#include <list>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+/*
+题目：
+    输入一个自然数 n，求表达式 f(n) = 1!*2!*3!*...*n! 的结果的末尾有几个连续的0？
+输入：
+    自然数 n
+输出：
+    f(n) 末尾连续的0的个数
+示例1：
+    输入输出示例仅供测试，后台判题依据一般不包含示例
+    输入：
+        11
+    输出：
+        9
+*/
+
+class Solution
+{
+public:
+
+};
+
+int main()
+{
+    Solution solution;
+
+    return 0;
+}
