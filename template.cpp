95acb4fcc43f3c7c18726a05379c356159e667d5
zhangrunjie <zhangrunjie@bytedance.com>
2018-09-16 09:44:25 +0800

    调整工作目录的结构
    修改 template.cpp 文件作为整个工作目录的模板；
    添加小红书笔试通过的题目。

---
+++ template.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include <vector>
+#include <queue>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+
+};
+
+int main()
+{
+    Solution solution;
+
+    return 0;
+}
