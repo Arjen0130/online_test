9ac3aae351e346aaf15eaf2ee89af080d246c2f5
zhangrunjie <zhangrunjie@bytedance.com>
2018-09-20 08:40:45 +0800

    添加了题目描述，添加了关键点的注释信息，这道题已经 accept

--- 2018-09-19-hanwuji/1_convert_time_format.cpp
+++ 2018-09-19-hanwuji/1_convert_time_format.cpp
@@ -12,6 +12,21 @@
 
 using namespace std;
 
+/*
+题目：
+    将12小时制的字符串转换为24小时制的字符串。
+输入：
+    一个描述12小时制时间的字符串。
+    所有的输入都是合理的，不用考虑输入不合理的情况。
+输出：
+    一个描述24小时制时间的字符串。
+示例：
+    输入：
+        08:03:45PM
+    输出：
+        20:03:45
+*/
+
 class Solution
 {
 public:
@@ -23,7 +38,10 @@ int main()
     Solution solution;
     string str;
     cin >> str;
-    if ('P' == str[8] || 'p' == str[8])
+
+    // 处理下午 PM 的时间
+    // 对 12:30:00PM 这种格式特殊处理
+    if (('P' == str[8] || 'p' == str[8]) && ('1' != str[0] && '2' != str[1]))
     {
         int h = str[0] - '0';
         int l = str[1] - '0';
@@ -32,6 +50,12 @@ int main()
         l %= 10;
         str[0] = char(h + '0');
         str[1] = char(l + '0');
+    }
+    // 对 12:30:00AM 这种格式特殊处理
+    if (('A' == str[8] || 'a' == str[8]) && ('1' == str[0] && '2' == str[1]))
+    {
+        str[0] = '0';
+        str[1] = '0';
     }
     str = str.substr(0, 8);
     cout << str << endl;
