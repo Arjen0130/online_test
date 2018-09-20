e8dd33cb37ca904512c50a24ae01b7112b8f5532
zhangrunjie <zhangrunjie@bytedance.com>
2018-09-20 08:56:38 +0800

    添加了题目描述，但还没有解题思路

---
+++ 2018-09-19-hanwuji/4_find_max_length_of_subcllection.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+// STL head file
+#include <vector>
+#include <queue>
+#include <list>
+#include <map>
+#include <algorithm>
+// Normal header files in C
+#include <ctime>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+/*
+题目：
+    给定一个由正整数组成的集合 S，找出一个最大的子集合，使得 S 中任意两个数字的和都不能被 K 整除。
+    例如， S = [10, 10, 12, 19, 22, 24, 25]， K = 4。
+    则 S 的可能取值为 [10, 19, 25]， 或者 [19, 22, 25] 等。
+    S 最多只能取3个数。
+输入：
+    输入为两行。
+    第一行两个数字，分别表示集合 S 的元素数量 N 和 K。
+    第二行为 N 个数字，分别是 S 的各个元素值。
+    1 < N < 1e5
+    1 < K < 100
+    1 < S[i] < 1e9
+输出：
+    输出为一个数字，即集合 S 的最大长度。
+*/
+
+class Solution
+{
+public:
+
+};
+
+int main()
+{
+    Solution solution;
+
+    return 0;
+}
